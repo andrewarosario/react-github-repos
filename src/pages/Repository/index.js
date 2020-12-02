1cbc4ca05d339a41a58105a40beeff44520ca29e
andrewarosario <andrew.arosario@gmail.com>
2020-12-02 14:42:31 -0300

    realizando requisição do repositório selecionado

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
-export default function Repository() {
+export default function Repository(props) {
+
+  const [ repository, setRepository ] = useState({});
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      const { match } = props;
+      const response = await api.get(`/repos/${match.params.user}/${match.params.repository}`);
+      setRepository(response.data);
+    }
+
+    makeRequest();
+    
+  }, [])
+
   return <h1>Repositório</h1>;
 }
