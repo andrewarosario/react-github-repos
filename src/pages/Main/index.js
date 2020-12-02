0024aaca0771d634b88973a2c076371922e60079
Andrew Rosário <andrew.arosario@gmail.com>
2020-12-02 00:58:11 -0300

    criando paginação

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaSearch, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SearchButton, List } from './styles';
+import { GoArrowLeft, GoArrowRight } from 'react-icons/go';
+import { Container, Form, SearchButton, List, PageNav } from './styles';
 import api from '../../services/api';
+import useDebounce from '../../helpers/use-debounce'
 
 export default function Main() {
   const [ loading, setLoading ] = useState(false);
   const [ search, setSearch ] = useState('');
+  const [ page, setPage ] = useState(1);
   const [ repositories, setRepositories ] = useState([]);
+  const debouncedSearch = useDebounce(search, 500);
+
+  useEffect(() => {
+
+    const getResponse = () => {
+        return api.get('/search/repositories', {
+            params: {
+                q: debouncedSearch,
+                per_page: 10,
+                page
+            }
+        })
+    }
+
+    const listRepositories = async () =>  {
+        if (!debouncedSearch) return;
+
+        setLoading(true);
+        const response = await getResponse();
+        setRepositories(response.data.items);
+        setLoading(false);
+    }
+
+    listRepositories();
+  }, [debouncedSearch, page]);
 
   const handleSubmit = async e => {
     e.preventDefault();
-    setLoading(true);
-    const response = await api.get(`/search/repositories?q=${search}`);
-    setRepositories(response.data.items);
-    setLoading(false);
+    setPage(1);
   }
 
   const handleInputChange = e => {
       setSearch(e.target.value);
   }
+
+  const handlePage = action => {
+    const newPage = action === 'back' ? page - 1 : page + 1;
+    setPage(newPage);
+  };
 
   return (
     <Container>
@@ -51,11 +81,27 @@ export default function Main() {
             <div>
             <Link to={`/repository/${encodeURIComponent(repo.full_name)}`}>
                 <img src={repo.owner.avatar_url} alt={repo.owner.name} />
-                <span>{repo.name}</span>
+                <span>{repo.full_name}</span>
             </Link>
             </div>
         </li>
         ))}
+        {repositories.length > 0 &&
+          <PageNav>
+            <button
+              type="button"
+              disabled={page < 2}
+              onClick={() => handlePage('back')}
+            >
+              <GoArrowLeft />
+              Página Anterior
+            </button>
+            <button type="button" onClick={() => handlePage('next')}>
+              Próxima Página
+              <GoArrowRight />
+            </button>
+          </PageNav>
+        }
       </List>
       
     </Container>
