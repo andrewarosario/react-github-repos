f5c974339c592976338340c11bba716b023c1b42
Andrew Rosário <andrew.arosario@gmail.com>
2020-12-01 21:01:43 -0300

    estilizando página principal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Title } from './styles';
+import { FaGithubAlt, FaSearch } from 'react-icons/fa';
+import { Container, Form, SearchButton } from './styles';
 
 export default function Main() {
-    return (
-        <Title>
-          Repositórios do Github
-        </Title>
-      );
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios do Github
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Buscar repositórios" />
+
+        <SearchButton>
+          <FaSearch color="#FFF" size={14} />
+        </SearchButton>
+      </Form>
+    </Container>
+  );
 }
