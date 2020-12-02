9da4e88cc3e66afbc92ca8b3ebf0128614dc5e86
Andrew Rosário <andrew.arosario@gmail.com>
2020-12-01 22:25:36 -0300

    renderizando repositórios buscados

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaSearch, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SearchButton } from './styles';
+import { Container, Form, SearchButton, List } from './styles';
 import api from '../../services/api';
 
 export default function Main() {
   const [ loading, setLoading ] = useState(false);
   const [ search, setSearch ] = useState('');
+  const [ repositories, setRepositories ] = useState([]);
 
   const handleSubmit = async e => {
     e.preventDefault();
     setLoading(true);
     const response = await api.get(`/search/repositories?q=${search}`);
-    console.log(response);
+    setRepositories(response.data.items);
     setLoading(false);
   }
 
@@ -42,6 +44,20 @@ export default function Main() {
           )}
         </SearchButton>
       </Form>
+
+      <List>
+        {repositories.map(repo => (
+        <li key={repo.full_name}>
+            <div>
+            <Link to={`/repository/${encodeURIComponent(repo.full_name)}`}>
+                <img src={repo.owner.avatar_url} alt={repo.owner.name} />
+                <span>{repo.name}</span>
+            </Link>
+            </div>
+        </li>
+        ))}
+      </List>
+      
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -47,3 +47,39 @@ export const SearchButton = styled.button.attrs({
   justify-content: center;
   align-items: center;
 `;
+
+export const List = styled.ul`
+  margin-top: 30px;
+  list-style-type: none;
+  font-size: 16px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    img {
+      width: 32px;
+      margin-right: 12px;
+      border-radius: 50%;
+      border: 2px solid #dbdbdb;
+    }
+
+    a {
+      display: flex;
+      align-items: center;
+      color: inherit;
+      text-decoration: none;
+
+      &:hover {
+        color: #7159c1;
+      }
+    }
+  }
+`;
